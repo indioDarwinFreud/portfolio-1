3dfe393b43571b9412f2afd70b3f21c0fd2ad813
Luis Lovos <luisestebanlovos@gmail.com>
2025-02-13 22:19:07 -0300

    Update

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -1,35 +1,36 @@
 import Projects from "@/components/projects";
-
-
+import TransitionPage from "@/components/transition-page";
+import CircleImage from "@/components/circle-image"
 
 const ProjectsPage = () => {
     return (
         <>
-        
-        <div className="grid items-center justify-center h-screen max-w-5xl 
+            <TransitionPage />
+            <CircleImage/>
+            <div className="grid items-center justify-center h-screen max-w-5xl 
         gap-5 mx-auto md:grid-cols-2 md:px-50">
-            <div className="max-w-[450px] mt-20 md:mt-0">
-                <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
-                    HOME{" "}
-                    <span className="font-bold text-secondary">
-                        GYM.
-                    </span>
-                </h1>
-                <p className="mb:3 text-xl text-gray-300">
-                    Es una App destinada para todas aquellas personas que necesiten llevar su vida a otro nivel.
-                    La App ofrece una amplia variedad de funciones, donde cada cliente contará con el seguimiento de profesionales, en el tema de la gastronomía saludable, y el ejercicio (nutricionistas y preparadores físicos).
+                <div className="max-w-[450px] mt-20 md:mt-0">
+                    <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
+                        HOME{" "}
+                        <span className="font-bold text-secondary">
+                            GYM.
+                        </span>
+                    </h1>
+                    <p className="mb:3 text-xl text-gray-300">
+                        Es una App destinada para todas aquellas personas que necesiten llevar su vida a otro nivel.
+                        La App ofrece una amplia variedad de funciones, donde cada cliente contará con el seguimiento de profesionales, en el tema de la gastronomía saludable, y el ejercicio (nutricionistas y preparadores físicos).
 
-                </p>
+                    </p>
+
+                </div>
+                {/* SLIDER (con esto se puede hacer notas en react) */}
+                <div>
+                    <Projects />
+                </div>
 
             </div>
-            {/* SLIDER (con esto se puede hacer notas en react) */}
-            <div>
-            <Projects />
-            </div>
-
-        </div>
         </>
-        
+
     )
 }
 
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,19 +1,23 @@
-
+import Image from "next/image";
 
 const Projects = () => {
   return (
-    <div className="flex px-6 py-8 h-auto md:h-[290px] rounded-lg 
-                cursor-pointer bg-[rgba(65,47,123,0.15)] sm:flex-col gap-x-6 sm:gap-x-0 
+    <div className="relative flex px-6 py-8 h-[290px] rounded-lg 
+                cursor-pointer bg-[rgba(65,47,123,0.15)] sm:flex-col 
                 group hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300 
-                hover:border-secondary border-2">
-      <div className="mb-4 text-4xl text-secondary">{ }</div>
-      <div>
-        <h3 className="mb-4 text-lg">{ }</h3>
-        <p className="text-sm">{ }</p>
-      </div>
+                hover:border-secondary border-2 overflow-hidden">
+      
+      {/* Imagen de fondo ajustada al tamaño del cuadro */}
+      <Image 
+        src="/HomeGym.PNG" 
+        layout="fill" 
+        objectFit="cover" 
+        alt="Home Gym"
+        className="absolute top-0 left-0 w-full h-full rounded-lg"
+      />
     </div>
   );
-
 }
 
 export default Projects;
+
