7beac1a050705f43bbef4e00aee872b148d5e2f9
Luis Lovos <luisestebanlovos@gmail.com>
2025-03-11 21:12:05 -0300

    Update

--- app/(routes)/portfolio/page.tsx
+++ app/(routes)/portfolio/page.tsx
@@ -18,7 +18,7 @@ const PortfolioPage = () => {
                     <span className="text-secondary fond-bold"> trabajos realizados</span>
                     </h1>
 
-                <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-4">
+                <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-2">
                     {dataPortfolio.map((data) => (
                         <PortfolioBox key={data.id} data={data} />
                     ))}
