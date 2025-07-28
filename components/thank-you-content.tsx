73f88a5eb4e6e948b477fe9620d75b44b0c17e53
Luis Lovos <luisestebanlovos@gmail.com>
2025-07-28 20:04:14 -0300

    Update

--- components/thank-you-content.tsx
+++ components/thank-you-content.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
 const ThankYouContent = () => {
   return (
     <div className="text-center text-white mt-20">
       <h1 className="text-4xl font-bold mb-4">¡Gracias por contactarte!</h1>
       <p className="text-xl">Nos pondremos en contacto a la brevedad.</p>
-      <a href="/" className="underline text-secondary mt-6 block text-lg">
-        Volver al inicio
-      </a>
+      <Link href="/">
+        <span className="underline text-secondary mt-6 block text-lg">
+          Volver al inicio{" "}
+        </span>
+      </Link>
     </div>
   );
 };
